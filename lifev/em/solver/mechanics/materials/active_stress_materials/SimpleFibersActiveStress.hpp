7f782628c5303a3a5a2758598234df4e460ddd03
Simone Rossi <simone.rossi@epfl.ch>
2014-05-15 17:39:13 +0200

    Active Stress material

---
+++ lifev/em/solver/mechanics/materials/active_stress_materials/SimpleFibersActiveStress.hpp
@@ -0,0 +1,58 @@
+/*
+ * EMPassiveNeoHookean.hpp
+ *
+ *  Created on: 29/apr/2014
+ *      Author: srossi
+ */
+
+#ifndef SIMPLEFIBERSACTIVESTRESS_HPP_
+#define SIMPLEFIBERSACTIVESTRESS_HPP_
+
+
+#include <lifev/em/solver/mechanics/materials/EMMaterialType.hpp>
+
+//#include <lifev/em/solver/mechanics/materials/EMMaterialFunctions.hpp>
+
+
+
+//class EMPassiveNeoHookean : public virtual EMPassiveMaterial
+//{
+//public:
+//	EMPassiveNeoHookean();
+//	virtual ~EMPassiveNeoHookean() {}
+//};
+
+namespace LifeV
+{
+
+template<typename Mesh>
+class SimpleFibersActiveStress : public virtual EMMaterialType<Mesh>
+{
+public:
+	typedef EMMaterialType<Mesh> super;
+
+	SimpleFibersActiveStress();
+	virtual ~SimpleFibersActiveStress() {}
+};
+
+template<typename Mesh>
+SimpleFibersActiveStress<Mesh>::SimpleFibersActiveStress() :
+		super("Simple Fibers Active Stress", 1)
+{
+	this -> M_materialFunctionList[0].reset(new MaterialFunctions::SimpleActiveStress<Mesh>(10000.0)  );
+}
+
+
+template <typename MeshType>
+inline EMMaterialType<MeshType>* createSimpleFibersActiveStress()
+{
+    return new SimpleFibersActiveStress<MeshType>();
+}
+namespace
+{
+static bool registerEM_SimpleFibersActiveStress = EMMaterialType<LifeV::RegionMesh<LinearTetra> >::EMMaterialFactory::instance().registerProduct ("SimpleActiveStress", &createSimpleFibersActiveStress<LifeV::RegionMesh<LinearTetra> > );
+}
+
+}//LifeV
+
+#endif /* EMPASSIVENEOHOOKEAN_HPP_ */
