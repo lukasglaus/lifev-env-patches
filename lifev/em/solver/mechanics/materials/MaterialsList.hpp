fae55f5f7132886d59d9dc981c024b7ee9ec427e
Simone Rossi <simone.rossi@epfl.ch>
2014-05-15 17:38:31 +0200

    Material list added

---
+++ lifev/em/solver/mechanics/materials/MaterialsList.hpp
@@ -0,0 +1,19 @@
+/*
+ * PassiveMaterialsList.hpp
+ *
+ *  Created on: 30/apr/2014
+ *      Author: srossi
+ */
+
+#ifndef MATERIALSLIST_HPP_
+#define MATERIALSLIST_HPP_
+
+
+
+#include <lifev/em/solver/mechanics/materials/passive_materials/PassiveNeoHookean.hpp>
+#include <lifev/em/solver/mechanics/materials/passive_materials/PassiveIsotropicExponential.hpp>
+#include <lifev/em/solver/mechanics/materials/passive_materials/PassiveLinearizedNeoHookean.hpp>
+#include <lifev/em/solver/mechanics/materials/active_stress_materials/SimpleFibersActiveStress.hpp>
+
+
+#endif /* PASSIVEMATERIALSLIST_HPP_ */
