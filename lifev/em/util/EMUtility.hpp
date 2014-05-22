a1ab21353989818e331353970b665890d96a883b
Simone Rossi <simone.rossi@epfl.ch>
2014-05-22 15:32:09 +0200

    EM utility: new functions

--- lifev/em/util/EMUtility.hpp
+++ lifev/em/util/EMUtility.hpp
@@ -39,15 +39,23 @@
 #define EMUTILITY_H 1
 
 #include <lifev/core/LifeV.hpp>
-#include <lifev/core/array/VectorEpetra.hpp>
-#include <lifev/core/array/MatrixEpetra.hpp>
-#include <lifev/core/array/MapEpetra.hpp>
-#include <lifev/core/filter/ExporterEnsight.hpp>
+//#include <lifev/core/array/VectorEpetra.hpp>
+//#include <lifev/core/array/MatrixEpetra.hpp>
+//#include <lifev/core/array/MapEpetra.hpp>
+//#include <lifev/core/filter/ExporterEnsight.hpp>
 #include <lifev/core/filter/ExporterHDF5.hpp>
-#include <lifev/core/filter/ExporterEmpty.hpp>
-#include <lifev/core/filter/Exporter.hpp>
+//#include <lifev/core/filter/ExporterEmpty.hpp>
+//#include <lifev/core/filter/Exporter.hpp>
 #include <lifev/core/mesh/RegionMesh.hpp>
 #include <lifev/core/fem/FESpace.hpp>
+#include <Epetra_MpiComm.h>
+#include <lifev/core/filter/ExporterHDF5.hpp>
+#include <lifev/core/filter/GetPot.hpp>
+#include <boost/shared_ptr.hpp>
+
+
+
+#include <sys/stat.h>
 
 namespace LifeV
 {
@@ -56,13 +64,52 @@ namespace LifeV
 
 namespace EMUtility
 {
-
 //! EMUtility -
 /*!
  *  @author(s) Simone Rossi
  *
  *
  */
+
+std::string createOutputFolder(GetPot& command_line, Epetra_Comm& comm)
+{
+//	std::string problemFolder = "Output";//command_line.follow("Output", 2, "-o","--output");//) command_line.follow ( "Output", 2, "-o", "--output" );
+	std::string problemFolder = command_line.follow("Output", 2, "-o","--output");//) command_line.follow ( "Output", 2, "-o", "--output" );
+	// Create the problem folder
+	std::cout << "EMU - creating output folder: " << problemFolder << "\n";
+	if ( problemFolder.compare ("./") )
+	{
+		problemFolder += "/";
+
+		if ( comm.MyPID() == 0 )
+		{
+			mkdir ( problemFolder.c_str(), 0777 );
+		}
+	}
+	return problemFolder;
+}
+
+template<class Mesh>
+void setupExporter( ExporterHDF5<Mesh>& exporter,
+		            boost::shared_ptr<Mesh> localMeshPtr,
+		            boost::shared_ptr<Epetra_Comm> commPtr,
+		            std::string fileName,
+		            std::string folder)
+{
+    exporter.setMeshProcId (localMeshPtr, commPtr->MyPID() );
+    exporter.setPrefix (fileName);
+    exporter.exportPID (localMeshPtr, commPtr);
+    exporter.setPostDir (folder);
+}
+
+
+
+
+
+
+
+
+
 void EpetraSqrt ( VectorEpetra& vec)
 {
     Int size = vec.epetraVector().MyLength();
@@ -88,6 +135,40 @@ MatrixSmall<3, 3> identity()
     I (2, 2) = 1.0;
     return I;
 }
+
+
+
+
+
+class Normalize
+{
+public:
+    typedef LifeV::VectorSmall<3> return_Type;
+
+    return_Type operator() (const LifeV::VectorSmall<3>& a)
+    {
+        LifeV::VectorSmall<3>   n;
+
+        LifeV::Real norm = std::sqrt (a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+        if ( norm != 0 )
+        {
+            n[0] = a[0] / norm;
+            n[1] = a[1] / norm;
+            n[2] = a[2] / norm;
+        }
+        else
+        {
+            n[0] = 1.0;
+            n[1] = 0.0;
+            n[2] = 0.0;
+        }
+        return n;
+    }
+
+    Normalize() {}
+    ~Normalize() {}
+};
+
 
 } // namespace EMUtility
 
