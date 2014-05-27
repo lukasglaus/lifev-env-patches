76b32e0f59c59b4d4822152195d33f5414ea2520
Simone Rossi <simone.rossi@epfl.ch>
2014-05-27 19:24:31 +0200

    Small updates in EM material type

--- lifev/em/solver/mechanics/materials/MaterialsList.hpp
+++ lifev/em/solver/mechanics/materials/MaterialsList.hpp
@@ -9,13 +9,15 @@
 #define MATERIALSLIST_HPP_
 
 
-
+#include <lifev/em/solver/mechanics/materials/passive_materials/PassiveHolzapfelOgden.hpp>
+#include <lifev/em/solver/mechanics/materials/passive_materials/PassiveOrthotropicFung.hpp>
 #include <lifev/em/solver/mechanics/materials/passive_materials/PassiveNeoHookean.hpp>
 #include <lifev/em/solver/mechanics/materials/passive_materials/PassiveIsotropicExponential.hpp>
 #include <lifev/em/solver/mechanics/materials/passive_materials/PassiveIsotropicExponentialWithShear.hpp>
 #include <lifev/em/solver/mechanics/materials/passive_materials/PassiveLinearizedNeoHookean.hpp>
 #include <lifev/em/solver/mechanics/materials/active_stress_materials/SimpleFibersActiveStress.hpp>
 #include <lifev/em/solver/mechanics/materials/passive_materials/PassiveTransverselyIsotropicExponential.hpp>
+#include <lifev/em/solver/mechanics/materials/passive_materials/PassiveTransverselyIsotropicFung.hpp>
 
 
 #endif /* PASSIVEMATERIALSLIST_HPP_ */
