c85c311312c64aca88e17a00232ba355f45aa2c8
Simone Rossi <simone.rossi@epfl.ch>
2014-10-25 09:17:02 -0400

    Added Active Strain Models

---
+++ lifev/em/solver/activation/activeStrainModels/ActiveStrainRossiModel14.hpp
@@ -0,0 +1,78 @@
+/*
+ * RossiModel14.hpp
+ *
+ *  Created on: 13/mag/2014
+ *      Author: srossi
+ */
+
+#ifndef ACTIVESTRAINROSSIMODEL14_HPP_
+#define ACTIVESTRAINROSSIMODEL14_HPP_
+
+#include <lifev/em/solver/activation/activeStressModels/ActiveStrainActivation.hpp>
+
+namespace LifeV
+{
+
+class ActiveStrainRossiModel14 : public virtual ActiveStrainActivation
+{
+public:
+
+	enum ActivationAnisotropy { TransversilyIsotropic, Orthotropic };
+    //! Distributed vector // For parallel usage
+    typedef VectorEpetra                                                vector_Type;
+
+    typedef boost::shared_ptr<VectorEpetra>                             vectorPtr_Type;
+
+    typedef ActiveStrainActivation                                      super;
+
+    ActiveStrainRossiModel14 ( MapEpetra& map,
+    		                   Real inverseViscosity = 0.0001,
+    		                   Real coefficient = 1.,
+    		                   Real threshold = 0.21 );
+    ActiveStrainRossiModel14 ( const MapEpetra& map,
+    		                   Real inverseViscosity = 0.0001,
+    		                   Real coefficient = 1.,
+    		                   Real threshold = 0.21  );
+
+    virtual ~ActiveStrainRossiModel14() {}
+
+//    void solveModel (VectorEpetra& ActiveStress, Real timeStep);
+
+    void solveModel (VectorEpetra& displacement, VectorEpetra& Calcium, Real timeStep, ActivationAnisotropy type = Orthotropic);
+
+    void computeTransversalActivation( ActivationAnisotropy  anisotropy = Orthotropic )
+
+    Real computeActiveStress(Real i4f, Real Calcium);
+
+
+    Real inverseViscosity()
+    {
+        return M_inverseViscosity;
+    }
+
+    void setInverseViscosity (Real inverseViscosity)
+    {
+        M_inverseViscosity = inverseViscosity;
+    }
+
+    Real activeForceCoefficient()
+    {
+        return M_activeForceCoefficient;
+    }
+
+    void setActiveForceCoefficient (Real coefficient)
+    {
+        M_activeForceCoefficient = coefficient;
+    }
+
+
+private:
+
+    Real M_inverseViscosity;
+    Real M_activeForceCoefficient;
+    Real M_chemicalThreshold;
+};
+
+} /* namespace LifeV */
+
+#endif /* ROSSIMODEL14_HPP_ */
