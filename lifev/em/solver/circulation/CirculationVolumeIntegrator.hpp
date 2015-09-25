7362668158717df791c970754458294ad8841378
thomaskummer <kummetho@gmail.com>
2015-09-25 11:00:54 +0200

    debug volume integration for P2 elements

--- lifev/em/solver/circulation/CirculationVolumeIntegrator.hpp
+++ lifev/em/solver/circulation/CirculationVolumeIntegrator.hpp
@@ -200,7 +200,7 @@ public:
             BOOST_AUTO_TPL (FmT, minusT (F) );
             BOOST_AUTO_TPL (J, det (F) );
             
-            QuadratureBoundary myBDQR (buildTetraBDQR (quadRuleTria4pt) );
+            QuadratureBoundary myBDQR (buildTetraBDQR (quadRuleTria1pt) );
 
             *intergral *= 0.0;
             integrate (boundary (M_localMeshPtr, bdFlag), myBDQR, M_ETFESpace,
