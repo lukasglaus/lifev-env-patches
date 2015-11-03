9bd6d085d4ccc6de55dff50699dbcbdb0e6508c1
thomaskummer <kummetho@gmail.com>
2015-11-03 15:51:14 +0100

    biVent

--- lifev/em/examples/example_EMBiVent/main.cpp
+++ lifev/em/examples/example_EMBiVent/main.cpp
@@ -50,7 +50,7 @@ using namespace LifeV;
 
 Real Iapp (const Real& t, const Real&  X, const Real& Y, const Real& Z, const ID& /*i*/)
 {
-    return ( Y > 1.5 && Y < 3 /*std::abs(X) < 1 && std::abs(Z-3) < 1 && Y < 0*/ /*&& Y < 0.25 && Z < 0.25 */ && t < 7 && t > 5 ? 30 : 0 );
+    return ( Y > 1.5 && Y < 3 /*std::abs(X) < 1 && std::abs(Z-3) < 1 && Y < 0*/ /*&& Y < 0.25 && Z < 0.25 */ && ( (t < 7 && t > 5) ||  (t < 807 && t > 805) ) ? 30 : 0 );
     // setAppliedCurrent in electrophys. module.
 }
 
