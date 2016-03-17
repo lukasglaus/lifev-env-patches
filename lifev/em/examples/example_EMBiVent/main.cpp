b9c2f0b40c394a015508a5a5369a8831c46686ca
thomaskummer <kummetho@gmail.com>
2016-03-17 08:56:40 +0100

    meshName from command line

--- lifev/em/examples/example_EMBiVent/main.cpp
+++ lifev/em/examples/example_EMBiVent/main.cpp
@@ -73,7 +73,7 @@ private:
 
 Real Iapp (const Real& t, const Real&  X, const Real& Y, const Real& Z, const ID& /*i*/)
 {
-    bool coords ( Y > 2.5 && Y < 4 );
+    bool coords ( Y > 1.5 && Y < 3 );
     bool time ( fmod(t, 800.) < 4 && fmod(t, 800.) > 2);
     return ( coords && time ? 30 : 0 );
     // setAppliedCurrent in electrophys. module.
