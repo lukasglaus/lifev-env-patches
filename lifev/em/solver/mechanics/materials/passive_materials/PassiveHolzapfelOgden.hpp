724da940c55cbece51d34a2e151985c188eb41f2
thomaskummer <kummetho@gmail.com>
2016-10-05 12:36:47 +0200

    cervin lifev update

--- lifev/em/solver/mechanics/materials/passive_materials/PassiveHolzapfelOgden.hpp
+++ lifev/em/solver/mechanics/materials/passive_materials/PassiveHolzapfelOgden.hpp
@@ -44,7 +44,7 @@ PassiveHolzapfelOgden<Mesh>::PassiveHolzapfelOgden() :
     //this -> M_materialFunctionList[2].reset ( new MaterialFunctions::IsotropicExponential<Mesh> (3330.0, 9.242)  );
     //this -> M_materialFunctionList[3].reset ( new MaterialFunctions::dIsotropicExponential<Mesh> (3330.0, 9.242)  );
     //this -> M_materialFunctionList[0].reset ( new MaterialFunctions::AnisotropicExponential<Mesh> (185350, 15.972)  );
-    this -> M_materialFunctionList[1].reset ( new MaterialFunctions::dAnisotropicExponential<Mesh> (185350, 15.972)  );
+    //this -> M_materialFunctionList[1].reset ( new MaterialFunctions::dAnisotropicExponential<Mesh> (185350, 15.972)  );
     //this -> M_materialFunctionList[2].reset ( new MaterialFunctions::AnisotropicExponential<Mesh> ( 25640.0, 10.446, MaterialFunctions::AnisotropicExponential<Mesh>::Sheets )  );
     //this -> M_materialFunctionList[3].reset ( new MaterialFunctions::dAnisotropicExponential<Mesh> ( 25640.0, 10.446, MaterialFunctions::AnisotropicExponential<Mesh>::Sheets )  );
     //this -> M_materialFunctionList[4].reset ( new MaterialFunctions::ShearExponential<Mesh> (4170.0, 11.602)  );
