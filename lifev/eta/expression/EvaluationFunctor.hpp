db7b35ffe974b73de82fe26ef778971400a0bd1c
thomaskummer <kummetho@gmail.com>
2017-03-03 10:51:05 +0100

    intrepid lifev update

--- lifev/eta/expression/EvaluationFunctor.hpp
+++ lifev/eta/expression/EvaluationFunctor.hpp
@@ -616,6 +616,228 @@ const flag_Type EvaluationFunctor3<FunctorType, Argument1EvaluationType, Argumen
     = Argument1EvaluationType::S_solutionUpdateFlag | Argument2EvaluationType::S_solutionUpdateFlag | Argument3EvaluationType::S_solutionUpdateFlag;
 
 
+    
+    
+    
+  
+    
+    
+    
+    //! Evaluation for a generic functor with 4 arguments
+    /*!
+     @author Simone Rossi <simone.rossi@epfl.ch>
+     
+     This class aims at representing a functor with 3 arguements during the assembly
+     
+     This class is an Evaluation class, and therefore, has all the methods
+     required to work within the Evaluation trees.
+     
+     <b> Template requirement </b>
+     
+     There are two template arguments for this class: The functor and the ArgumentEvaluationType.
+     
+     <i> FunctorType </i> The functor has to be copiable, must implement the operator() const, has
+     a type named return_Type, the operator() must return a return_Type.
+     
+     <i> Argument1EvaluationType </i> It has to be an Evaluation class.
+     
+     <i> Argument2EvaluationType </i> It has to be an Evaluation class.
+     
+     <i> Argument3EvaluationType </i> It has to be an Evaluation class.
+     
+     */
+    template <typename FunctorType, typename Argument1EvaluationType, typename Argument2EvaluationType, typename Argument3EvaluationType>, typename Argument4EvaluationType>
+    class EvaluationFunctor4
+    {
+    public:
+        
+        //! @name Public Types
+        //@{
+        
+        //! Type returned by this class
+        typedef typename FunctorType::return_Type return_Type;
+        
+        //@}
+        
+        
+        //! @name Static constants
+        //@{
+        
+        //! Flag for the global current FE
+        const static flag_Type S_globalUpdateFlag;
+        
+        //! Flag for the test current FE
+        const static flag_Type S_testUpdateFlag;
+        
+        //! Flag for the solution current FE
+        const static flag_Type S_solutionUpdateFlag;
+        
+        //@}
+        
+        
+        //! @name Constructors, destructor
+        //@{
+        
+        //! Copy constructor
+        EvaluationFunctor4 (const EvaluationFunctor4<FunctorType, Argument1EvaluationType, Argument2EvaluationType, Argument3EvaluationType, Argument4EvaluationType>& eval)
+        : M_functor (eval.M_functor),
+        M_evaluation1 (eval.M_evaluation1),
+        M_evaluation2 (eval.M_evaluation2),
+        M_evaluation3 (eval.M_evaluation3),
+        M_evaluation4 (eval.M_evaluation4)
+        {}
+        
+        //! Constructor from the corresponding expression
+        template<typename Argument1, typename Argument2, typename Argument3, typename Argument4>
+        explicit EvaluationFunctor4 (const ExpressionFunctor4<FunctorType, Argument1, Argument2, Argument3, Argument4>& expression)
+        : M_functor (expression.functor() ),
+        M_evaluation1 (expression.argument1() ),
+        M_evaluation2 (expression.argument2() ),
+        M_evaluation3 (expression.argument3() ),
+        M_evaluation4 (expression.argument4() ) {}
+        
+        //! Destructor
+        ~EvaluationFunctor4()
+        {}
+        
+        //@}
+        
+        
+        //! @name Methods
+        //@{
+        
+        //! Internal update
+        void update (const UInt& iElement)
+        {
+            M_evaluation1.update (iElement);
+            M_evaluation2.update (iElement);
+            M_evaluation3.update (iElement);
+            M_evaluation4.update (iElement);
+        }
+        
+        //! Display method
+        static void display (std::ostream& out = std::cout)
+        {
+            out << "f( ";
+            Argument1EvaluationType::display (out);
+            out << " , ";
+            Argument2EvaluationType::display (out);
+            out << " , ";
+            Argument3EvaluationType::display (out);
+            out << " )";
+            Argument4EvaluationType::display (out);
+            out << " )";
+        }
+        
+        //@}
+        
+        
+        //! @name Set Methods
+        //@{
+        
+        //! Setter for the global current FE
+        template< typename CFEType >
+        void setGlobalCFE (const CFEType* globalCFE)
+        {
+            M_evaluation1.setGlobalCFE (globalCFE);
+            M_evaluation2.setGlobalCFE (globalCFE);
+            M_evaluation3.setGlobalCFE (globalCFE);
+            M_evaluation4.setGlobalCFE (globalCFE);
+        }
+        
+        //! Setter for the test current FE
+        template< typename CFEType >
+        void setTestCFE (const CFEType* testCFE)
+        {
+            M_evaluation1.setTestCFE (testCFE);
+            M_evaluation2.setTestCFE (testCFE);
+            M_evaluation3.setTestCFE (testCFE);
+            M_evaluation4.setTestCFE (testCFE);
+        }
+        
+        //! Setter for the solution current FE
+        template< typename CFEType >
+        void setSolutionCFE (const CFEType* solutionCFE)
+        {
+            M_evaluation1.setSolutionCFE (solutionCFE);
+            M_evaluation2.setSolutionCFE (solutionCFE);
+            M_evaluation3.setSolutionCFE (solutionCFE);
+            M_evaluation4.setSolutionCFE (solutionCFE);
+        }
+        
+        //! Setter for the quadrature rule
+        void setQuadrature (const QuadratureRule& qr)
+        {
+            M_evaluation1.setQuadrature (qr);
+            M_evaluation2.setQuadrature (qr);
+            M_evaluation3.setQuadrature (qr);
+            M_evaluation4.setQuadrature (qr);
+        }
+        
+        //@}
+        
+        
+        //! @name Get Methods
+        //@{
+        
+        //! Getter for a value
+        return_Type value_q (const UInt& q) const
+        {
+            return (*M_functor) (M_evaluation1.value_q (q), M_evaluation2.value_q (q), M_evaluation3.value_q (q), M_evaluation4.value_q (q) );
+        }
+        
+        //! Getter for the value for a vector
+        return_Type value_qi (const UInt& q, const UInt& i) const
+        {
+            return (*M_functor) (M_evaluation1.value_qi (q, i), M_evaluation2.value_qi (q, i), M_evaluation3.value_qi (q, i),  M_evaluation4.value_qi (q, i) );
+        }
+        
+        //! Getter for the value for a matrix
+        return_Type value_qij (const UInt& q, const UInt& i, const UInt& j) const
+        {
+            return (*M_functor) (M_evaluation1.value_qij (q, i, j), M_evaluation2.value_qij (q, i, j), M_evaluation3.value_qij (q, i, j), M_evaluation4.value_qij (q, i, j) );
+        }
+        
+        //@}
+        
+    private:
+        
+        
+        //! @name Private Methods
+        //@{
+        
+        //! No empty constructor
+        EvaluationFunctor3();
+        
+        //@}
+        
+        // Internal storage
+        boost::shared_ptr<FunctorType> M_functor;
+        Argument1EvaluationType M_evaluation1;
+        Argument2EvaluationType M_evaluation2;
+        Argument3EvaluationType M_evaluation3;
+        Argument4EvaluationType M_evaluation4;
+    };
+    
+    
+    template<typename FunctorType, typename Argument1EvaluationType, typename Argument2EvaluationType, typename Argument3EvaluationType>, typename Argument4EvaluationType>
+    const flag_Type EvaluationFunctor4<FunctorType, Argument1EvaluationType, Argument2EvaluationType, Argument3EvaluationType, Argument4EvaluationType>::S_globalUpdateFlag
+    = Argument1EvaluationType::S_globalUpdateFlag | Argument2EvaluationType::S_globalUpdateFlag | Argument3EvaluationType::S_globalUpdateFlag | Argument4EvaluationType::S_globalUpdateFlag;
+    
+    template<typename FunctorType, typename Argument1EvaluationType, typename Argument2EvaluationType, typename Argument3EvaluationType>, typename Argument4EvaluationType>
+    const flag_Type EvaluationFunctor3<FunctorType, Argument1EvaluationType, Argument2EvaluationType, Argument3EvaluationType, Argument4EvaluationType>::S_testUpdateFlag
+    = Argument1EvaluationType::S_testUpdateFlag | Argument2EvaluationType::S_testUpdateFlag | Argument3EvaluationType::S_testUpdateFlag  | Argument4EvaluationType::S_testUpdateFlag;
+    
+    template<typename FunctorType, typename Argument1EvaluationType, typename Argument2EvaluationType, typename Argument3EvaluationType>, typename Argument4EvaluationType>
+    const flag_Type EvaluationFunctor3<FunctorType, Argument1EvaluationType, Argument2EvaluationType, Argument3EvaluationType, Argument4EvaluationType>::S_solutionUpdateFlag
+    = Argument1EvaluationType::S_solutionUpdateFlag | Argument2EvaluationType::S_solutionUpdateFlag | Argument3EvaluationType::S_solutionUpdateFlag  | Argument4EvaluationType::S_solutionUpdateFlag;
+
+        
+    
+    
+    
+    
+    
 } // Namespace ExpressionAssembly
 
 } // Namespace LifeV
