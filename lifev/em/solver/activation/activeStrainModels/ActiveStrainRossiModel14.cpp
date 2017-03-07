23e9d6e34751810d9eeeb722ec6429a2451bfbb2
thomaskummer <kummetho@gmail.com>
2017-03-07 21:03:06 +0100

    euler lifev update

--- lifev/em/solver/activation/activeStrainModels/ActiveStrainRossiModel14.cpp
+++ lifev/em/solver/activation/activeStrainModels/ActiveStrainRossiModel14.cpp
@@ -68,48 +68,48 @@ ActiveStrainRossiModel14::solveModel ( Real& timeStep)
 
 }
 
-    void
-    ActiveStrainRossiModel14::solveModel2 ( Real& timeStep, boost::shared_ptr<RegionMesh<LinearTetra> > fullMeshPtr)
+void
+ActiveStrainRossiModel14::solveModel2 ( Real& timeStep, boost::shared_ptr<RegionMesh<LinearTetra> > fullMeshPtr)
+{
+    if(!M_I4fPtr)
     {
-        if(!M_I4fPtr)
-        {
-            ASSERT(false, " Cannot solve Active Strain Rossi model without I4f. ")
-            exit(-1);
-        }
-        Int nLocalDof = M_I4fPtr->epetraVector().MyLength();
-        
-        VectorSmall<3> X;
-        
-        for (int ik (0); ik < nLocalDof; ik++)
-        {
-            int iGID = M_I4fPtr->blockMap().GID (ik);
-            
-            X[0] = fullMeshPtr -> point(iGID).x();
-            X[1] = fullMeshPtr -> point(iGID).y();
-            X[2] = fullMeshPtr -> point(iGID).z();
-            
-            bool infarctZone = (X - M_PathologyCenter).norm() < M_PathologyRadius;
-            
-            Real Pa, dW;
-            Real i4f = (*M_I4fPtr)[iGID];
-            Real Ca = (*this->M_electroSolution.at(M_calciumIndex) )[iGID];
-            
-            Pa = computeActiveStress(i4f, Ca);
-            
-            Real g = (*M_fiberActivationPtr) [iGID];
-            Real g2 = g * g;
-            Real g3 = g * g2;
-            Real g4 = g * g3;
-            Real g5 = g * g4;
-            
-            dW = 2.0 * i4f * ( 3.0 * g - 6.0 * g2 + 10.0 * g3 - 15.0 * g4  + 21.0 * g5 );
-            Real grhs = M_inverseViscosity * ( Pa - dW ) / Ca / Ca;
-            grhs *= timeStep;
-            (*M_fiberActivationPtr) [iGID] += grhs;
-            (*M_fiberActivationPtr) [iGID] *= (infarctZone ? M_PathologyStrength : 1.0);
-        }
-        
+        ASSERT(false, " Cannot solve Active Strain Rossi model without I4f. ")
+        exit(-1);
     }
+    Int nLocalDof = M_I4fPtr->epetraVector().MyLength();
+    
+    VectorSmall<3> X;
+    
+    for (int ik (0); ik < nLocalDof; ik++)
+    {
+        int iGID = M_I4fPtr->blockMap().GID (ik);
+        
+        X[0] = fullMeshPtr -> point(iGID).x();
+        X[1] = fullMeshPtr -> point(iGID).y();
+        X[2] = fullMeshPtr -> point(iGID).z();
+        
+        bool infarctZone = (X - M_PathologyCenter).norm() < M_PathologyRadius;
+        
+        Real Pa, dW;
+        Real i4f = (*M_I4fPtr)[iGID];
+        Real Ca = (*this->M_electroSolution.at(M_calciumIndex) )[iGID];
+        
+        Pa = computeActiveStress(i4f, Ca);
+        
+        Real g = (*M_fiberActivationPtr) [iGID];
+        Real g2 = g * g;
+        Real g3 = g * g2;
+        Real g4 = g * g3;
+        Real g5 = g * g4;
+        
+        dW = 2.0 * i4f * ( 3.0 * g - 6.0 * g2 + 10.0 * g3 - 15.0 * g4  + 21.0 * g5 );
+        Real grhs = M_inverseViscosity * ( Pa - dW ) / Ca / Ca;
+        grhs *= timeStep;
+        (*M_fiberActivationPtr) [iGID] += grhs;
+        (*M_fiberActivationPtr) [iGID] *= (infarctZone ? M_PathologyStrength : 1.0);
+    }
+    
+}
 
 
 void
