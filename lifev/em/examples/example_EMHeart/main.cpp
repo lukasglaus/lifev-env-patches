6827ee1b4c755cf285d3774d430a44fd927d068b
thomaskummer <kummetho@gmail.com>
2017-03-22 14:22:00 +0100

    cervin lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -87,7 +87,7 @@ Real potentialMultiplyerFcn (const Real& t, const Real&  X, const Real& Y, const
 int main (int argc, char** argv)
 {
 
-//    feenableexcept(FE_INVALID | FE_OVERFLOW);
+    feenableexcept(FE_INVALID | FE_OVERFLOW);
     
     
     //============================================//
