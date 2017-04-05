036e8089ba93652289ed436568d8d44bb80fc27a
thomaskummer <kummetho@gmail.com>
2017-04-05 18:04:51 +0200

    cervin lifev update

--- lifev/em/solver/EMSolver.hpp
+++ lifev/em/solver/EMSolver.hpp
@@ -919,10 +919,10 @@ void
 EMSolver<Mesh, ElectroSolver>::solveActivation (Real dt)
 {
     
-    VectorEpetra a;
-    VectorEpetra b;
-    computeI4f (a, a, b, M_EMStructuralOperatorPtr->dispFESpacePtr());
-    //computeI4f (M_activationModelPtr->I4f(), *M_EMStructuralOperatorPtr->EMMaterial()->fiberVectorPtr(), *M_EMStructuralOperatorPtr->displacementPtr(), M_EMStructuralOperatorPtr->dispFESpacePtr());
+    //VectorEpetra a;
+    //VectorEpetra b;
+    //computeI4f (a, a, b, M_EMStructuralOperatorPtr->dispFESpacePtr());
+    computeI4f (M_activationModelPtr->I4f(), *M_EMStructuralOperatorPtr->EMMaterial()->fiberVectorPtr(), *M_EMStructuralOperatorPtr->displacementPtr(), M_EMStructuralOperatorPtr->dispFESpacePtr());
 
     M_activationModelPtr -> solveModelPathology ( dt, M_fullMeshPtr );
 }
