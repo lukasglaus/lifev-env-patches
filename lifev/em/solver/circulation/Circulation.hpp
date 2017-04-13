5e1d0f5d09b50326534bdc4c8ae8fe286ff54144
thomaskummer <kummetho@gmail.com>
2017-04-13 11:00:42 +0200

    euler lifev update

--- lifev/em/solver/circulation/Circulation.hpp
+++ lifev/em/solver/circulation/Circulation.hpp
@@ -119,8 +119,8 @@ public:
         if ( rank == 0 )
         {
             VectorStdDouble pMax (bcValues);
-            pMax[0] = bcValues[0]/(1-3.75e-4*solution(std::vector<std::string>{"lv", "sa"}));
-            pMax[1] = bcValues[1]/(1-1.4e-3*solution(std::vector<std::string>{"rv", "pa"}));
+            pMax[0] = bcValues[0]/(1-3.75e-4*solutionPrev0(std::vector<std::string>{"lv", "sa"}));
+            pMax[1] = bcValues[1]/(1-1.4e-3*solutionPrev0(std::vector<std::string>{"rv", "pa"}));
             
             unsigned int subiter (0);
             VectorEigen uPrevIter ( M_u );
@@ -135,8 +135,8 @@ public:
             {
                 ++subiter;
                 uPrevIter = M_u;
-                pMax[0] = bcValues[0]/(1-3.75e-4*solution(std::vector<std::string>{"lv", "sa"}));
-                pMax[1] = bcValues[1]/(1-1.4e-3*solution(std::vector<std::string>{"rv", "pa"}));
+                pMax[0] = bcValues[0]/(1-3.75e-4*solutionPrev0(std::vector<std::string>{"lv", "sa"}));
+                pMax[1] = bcValues[1]/(1-1.4e-3*solutionPrev0(std::vector<std::string>{"rv", "pa"}));
                 solve(dt, bcNames, pMax, 1, plotError, plotSystem);
                 residuum = ( M_u - uPrevIter );
                 
@@ -197,6 +197,22 @@ public:
         return M_u[ idx ];
     }
     
+    template<class type>
+    const double solutionPrev0(const type& bc)
+    {
+        DofHandler dofh(M_gv);
+        const unsigned int idx = dofh( bc );
+        return M_uPrev0[ idx ];
+    }
+
+    template<class type>
+    const double solutionPrev1(const type& bc)
+    {
+        DofHandler dofh(M_gv);
+        const unsigned int idx = dofh( bc );
+        return M_uPrev1[ idx ];
+    }
+
     void plotLinSys(const MatrixEigen& A, const VectorEigen& u, const VectorEigen& rhs)
     {
         DofHandler dofh(M_gv);
