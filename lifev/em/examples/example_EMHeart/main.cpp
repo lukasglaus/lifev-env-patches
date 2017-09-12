f6a6064bd1edc5601c468cbc16d03e5f3f12bbaa
thomaskummer <kummetho@gmail.com>
2017-09-12 13:39:29 +0200

    cervin lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -406,6 +406,7 @@ int main (int argc, char** argv)
 //        UInt kGID = p1PositionVector.blockMap().GID (j + 2 * p1nCompLocalDof);
         
         auto face = solver.fullMeshPtr()->face (j); //.x();
+        auto points = face.points();
         
         face.showMe();
         
