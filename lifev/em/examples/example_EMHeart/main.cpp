d2cf3f9eee640beb8ba8d7c3a86ca79f2ca2d46b
thomaskummer <kummetho@gmail.com>
2017-09-14 11:37:32 +0200

    cervin lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -362,8 +362,8 @@ int main (int argc, char** argv)
     auto createPatch = [] (EMSolver<RegionMesh<LinearTetra>, EMMonodomainSolver<RegionMesh<LinearTetra> > >& solver, const Vector3D& center, const Real& radius, const int& currentFlag, const int& newFlag)
     {
         std::cout << solver.mesh().size() << std::endl;
-        
-        for (auto& mesh : solver.mesh())
+        auto meshVector = solver.mesh();
+        for (auto& mesh : meshVector)
         {
             std::cout << "hi" << std::endl;
             mesh->showMe();
