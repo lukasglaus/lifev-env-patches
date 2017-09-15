448551f2ac26a307104ebc6b13d0d5d3aa3eafd0
thomaskummer <kummetho@gmail.com>
2017-09-15 15:17:41 +0200

    cervin lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -479,7 +479,8 @@ int main (int argc, char** argv)
     for (int i(0); i < 2; ++i)
     {
         vector_Type normalVector = normalEssentialBCVector(solver.fullMeshPtr(), solver.structuralOperatorPtr() -> dispFESpacePtr());
-        vectorPtr_Type normalVectorPtr(normalVector);
+        vectorPtr_Type normalVectorPtr;
+        normalVectorPtr.reset(normalVector);
         patchVecPtr.push_back(normalVectorPtr);
 //        *patchVecPtr[i] = 0.0;
         patchBCVecPtr.push_back ( bcVectorPtr_Type( new bcVector_Type( *patchVecPtr[i], solver.structuralOperatorPtr() -> dispFESpacePtr() -> dof().numTotalDof(), 1 ) ) );
