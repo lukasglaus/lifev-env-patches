164aac7ef32825d9447f61b48322eaaa1a6e00e0
thomaskummer <kummetho@gmail.com>
2017-09-28 11:36:28 +0200

    cervin lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -83,13 +83,13 @@ Real patchDispFun2 (const Real& t, const Real&  X, const Real& Y, const Real& Z,
     switch (i)
     {
         case 0:
-            return (-0.00005*t);
+            return (0.00005*t);
             break;
         case 1:
             return 0;
             break;
         case 2:
-            return (-0.00005*t);
+            return (0.00005*t);
             break;
         default:
             ERROR_MSG ("This entry is not allowed");
@@ -338,8 +338,6 @@ int main (int argc, char** argv)
     //============================================//
     Vector3D center1 {-0.7, -6.0, -4.7};
     Vector3D center2 {4.5, -6.0, 1.0};
-    Real radius1 = 2;
-    Real radius2 = 2;
     
     BCFunctionBase patchFun1 (patchDispFun1);
     BCFunctionBase patchFun2 (patchDispFun2);
@@ -353,7 +351,7 @@ int main (int argc, char** argv)
 //    patch1.setup(patchFunDirectional, center1, radius1);
     
     PatchCircleBCEssentialFull patch1(solver, "Patch1", 464, 100);
-    patch1.setup(patchFun1, center1, radius1);
+    patch1.setup(patchFun1, center1, 2.0);
     
 //    PatchCircleBCEssentialNormal patch2(solver, "Patch2", 464, 101);
 //    patch2.setup(patchFunNormal, center2, radius2);
@@ -362,7 +360,7 @@ int main (int argc, char** argv)
 //    patch1.setup(patchFunDirectional, center2, radius2);
 
     PatchCircleBCEssentialFull patch2(solver, "Patch2", 464, 101);
-    patch1.setup(patchFun2, center2, radius2);
+    patch1.setup(patchFun2, center2, 1.0);
     
     
     //============================================//
