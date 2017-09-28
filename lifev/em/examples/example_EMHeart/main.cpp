a72c4231c17ca6f14d4dddd397ebaf8337793922
thomaskummer <kummetho@gmail.com>
2017-09-28 12:06:36 +0200

    cervin lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -83,13 +83,13 @@ Real patchDispFun2 (const Real& t, const Real&  X, const Real& Y, const Real& Z,
     switch (i)
     {
         case 0:
-            return (0.00005*t);
+            return (-0.00005*t);
             break;
         case 1:
             return 0;
             break;
         case 2:
-            return (0.00005*t);
+            return (-0.00005*t);
             break;
         default:
             ERROR_MSG ("This entry is not allowed");
