696e2999cac82d0ad16a143dada083ee08de6b6d
thomaskummer <kummetho@gmail.com>
2017-10-02 11:54:00 +0200

    cervin lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -736,7 +736,7 @@ int main (int argc, char** argv)
         }
 
         
-//        solver.solveElectrophysiology (stim, t);
+        solver.solveElectrophysiology (stim, t);
 //        solver.solveActivation (dt_activation);
 
 //        modifyPatchBC(std::pow(std::sin(fmod(t, 800.) * 3.14159265359/300), 2), 0, 100);
