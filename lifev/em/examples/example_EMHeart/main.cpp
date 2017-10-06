fcb8c692c0340c2c3102a2a942618fe7192d621b
thomaskummer <kummetho@gmail.com>
2017-10-06 14:41:11 +0200

    intrepid lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -450,7 +450,7 @@ int main (int argc, char** argv)
     
     auto modifyEssentialVectorBC = [&] (const Real& time, const Real& factor)
     {
-        Real factor2 = 1e-2;
+        Real factor2 = 1e-1;
         directionVector = directionalVectorField(FESpace, direction1, time*factor2);
         directionBCVector.reset( new bcVector_Type( *directionVector, FESpace->dof().numTotalDof(), 1 ) );
         
