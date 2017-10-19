a625cb7bb0ded44d3509f3ccdef6624872bcf0ab
thomaskummer <kummetho@gmail.com>
2017-10-19 22:07:48 +0200

    cervin lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -149,7 +149,7 @@ int main (int argc, char** argv)
     std::string meshName = dataFile("solid/space_discretization/mesh_name", "cube4");
     std::string meshPath = dataFile("solid/space_discretization/mesh_dir", "../../data/mesh/");
     
-    solver.loadMesh (meshName + ".mesh", meshPath + meshPath + "/");
+    solver.loadMesh (meshName + ".mesh", meshPath + meshName + "/");
     
     
     //============================================//
