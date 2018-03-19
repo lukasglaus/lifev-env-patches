63615b197a2a2dfa8d710050678b7907382d3c90
thomaskummer <kummetho@gmail.com>
2018-03-19 12:42:36 +0100

    intrepid lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -554,51 +554,31 @@ int main (int argc, char** argv)
         const unsigned int restartInputStr = std::stoi(restartInput);
         const unsigned int nIter = (restartInputStr - 1) * dtExport / dt_mechanics;
         t = nIter * dt_mechanics;
-
-        std::cout << restartInputStr << std::endl;
-        std::cout << nIter << std::endl;
-        std::cout << t << std::endl;
-
-        
-        int resN = 0;
-        std::cout << ++resN << std::endl;
         
         // Set time exporter time index
-        heartSolver.exporter()->setTimeIndex(restartInputStr); // + 1);
-        std::cout << ++resN << std::endl;
+        //heartSolver.exporter()->setTimeIndex(restartInputStr); // + 1);
 
         // Load restart solutions from output files
         std::string polynomialDegree = dataFile ( "solid/space_discretization/order", "P2");
-        std::cout << ++resN << std::endl;
 
         for (int t_(0); t_ <= t; t_ = t_ + dtExport)
         {
-            
             std::string importNumber = "00000" + std::to_string(int(t_ / dtExport));
-            std::cout << importNumber << std::endl;
-
             importNumber = importNumber.substr(importNumber.length() - 5, importNumber.length());
-            std::cout << importNumber << std::endl;
-
+            std::cout << "TIME = " << t_ ": import frame " << importNumber << std::endl;
             
             ElectrophysiologyUtility::importVectorField ( solver.structuralOperatorPtr() -> displacementPtr(), "humanHeartSolution" , "Displacement", solver.localMeshPtr(), restartDir, polynomialDegree, importNumber );
-            std::cout << ++resN << std::endl;
 
             for ( unsigned int i = 0; i < solver.electroSolverPtr()->ionicModelPtr()->Size() ; ++i )
             {
                 ElectrophysiologyUtility::importScalarField (solver.electroSolverPtr()->globalSolution().at(i), "humanHeartSolution" , ("Ionic Variable " + std::to_string(i)), solver.localMeshPtr(), restartDir, polynomialDegree, importNumber );
             }
-            std::cout << ++resN << std::endl;
 
             ElectrophysiologyUtility::importScalarField (solver.activationModelPtr() -> fiberActivationPtr(), "humanHeartSolution" , "Activation", solver.localMeshPtr(), restartDir, polynomialDegree, importNumber );
-            std::cout << ++resN << std::endl;
 
             ElectrophysiologyUtility::importScalarField (solver.activationTimePtr(), "humanHeartSolution" , "Activation Time", solver.localMeshPtr(), restartDir, polynomialDegree, importNumber );
-            std::cout << ++resN << std::endl;
-
 
             circulationSolver.restartFromFile ( restartDir + "solution.dat" , int(t_/dt_mechanics) );
-            std::cout << ++resN << std::endl;
 
             heartSolver.postProcess(t_);
             circulationSolver.exportSolution( circulationOutputFile );
