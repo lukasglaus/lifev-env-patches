61a657cabd4d7a341b438a163f46442662f66c7f
thomaskummer <kummetho@gmail.com>
2018-04-17 17:36:19 +0200

    cervin lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -177,7 +177,9 @@ int main (int argc, char** argv)
     if ( 0 == comm->MyPID() ) std::cout << "Resizing mesh done" << '\r' << std::flush;
     if ( 0 == comm->MyPID() ) solver.fullMeshPtr()->showMe();
 
-    MeshUtility::p2MeshFromP1Data(*solver.localMeshPtr());
+    auto p2Mesh = *solver.localMeshPtr();
+    MeshUtility::p2MeshFromP1Data(p2Mesh);
+    
     
     //============================================
     // Create patches for essential patch b.c.
