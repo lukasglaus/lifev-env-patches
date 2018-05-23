5945332c00046cf7f497c71a8ec286162281d041
thomaskummer <kummetho@gmail.com>
2018-05-23 13:24:10 +0200

    euler lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -454,7 +454,7 @@ int main (int argc, char** argv)
         Real t_ = ( restoreAllPreviousTimesteps ? 0. : t );
         bool save ( std::abs(std::remainder(t, dt_save)) < 0.01 );
 
-        for (t_ ; std::abs(std::remainder(t_, t)) > 0.01 ; t_ += dtExport)
+        for (t_ ; t_ <= (t + 1e-3) ; t_ +=+ dtExport)
         {
             solver.structuralOperatorPtr() -> data() -> dataTime() -> setTime(t_);
 
@@ -485,7 +485,7 @@ int main (int argc, char** argv)
         }
         
         // Circulation export and/or restart
-        for (t_ = 0 ;  std::abs(std::remainder(t_, t)) > 0.01; t_ += dtExport)
+        for (t_ = 0 ; t_ <= (t + 1e-3) ; t_ += dtExport)
         {
             circulationSolver.restartFromFile ( restartDir + "solution.dat" , int(t_/dt_mechanics) );
             circulationSolver.exportSolution( circulationOutputFile );
