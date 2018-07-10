c0f6b90ca8ba734f4048f57ebffc7aa9d6a0ec1b
thomaskummer <kummetho@gmail.com>
2018-07-10 16:05:40 +0200

    euler lifev update

--- lifev/em/solver/HeartSolver.hpp
+++ lifev/em/solver/HeartSolver.hpp
@@ -350,7 +350,7 @@ public:
         M_emSolver.tensionEstimator().setDisplacement ( M_emSolver.structuralOperatorPtr()->displacement() );
         M_emSolver.tensionEstimator().analyzeTensionsRecoveryCauchyStresses();
         M_emSolver.tensionEstimator().analyzeTensionsRecoveryVonMisesStress();
-        M_emSolver.tensionEstimator().analyzeTensionsRecoveryEigenvalues();
+        //M_emSolver.tensionEstimator().analyzeTensionsRecoveryEigenvalues();
 
         // Compute deformed fiber direction
         M_emSolver.computeDeformedFiberDirection (M_emSolver.structuralOperatorPtr()->f(), *M_emSolver.structuralOperatorPtr()->EMMaterial()->fiberVectorPtr(), *M_emSolver.structuralOperatorPtr()->displacementPtr(), M_emSolver.structuralOperatorPtr()->dispFESpacePtr());
