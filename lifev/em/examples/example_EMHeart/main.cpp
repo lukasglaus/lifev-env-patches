1300dc9fa0bd68dd2ecd9d9cb54637619f8611f2
thomaskummer <kummetho@gmail.com>
2018-07-16 15:05:07 +0200

    intrepid lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -195,8 +195,7 @@ int main (int argc, char** argv)
     for ( UInt i (0) ; i < nPatchBC ; ++i )
     {
         std::string patchName = dataFile ( ( "solid/boundary_conditions/listEssentialPatchBC" ), " ", i );
-        std::string patchTypeString = "solid/boundary_conditions/listEssentialPatchBC/" + patchName + "/type";
-        std::string patchType = dataFile (patchTypeString, " ");
+        std::string patchType = dataFile ( "solid/boundary_conditions/listEssentialPatchBC/" + patchName + "/type" );
         patchBC.push_back(CREATE(EssentialPatchBC, patchType));
         patchBC[i]->setup(dataFile, patchName);
         patchBC[i]->createPatchArea(solver, 900 + i);
