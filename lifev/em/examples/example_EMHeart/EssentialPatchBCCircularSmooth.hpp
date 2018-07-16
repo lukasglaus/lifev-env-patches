fcb2258454ecae2bf61c98b41e0971d1453aa721
thomaskummer <kummetho@gmail.com>
2018-07-16 16:38:59 +0200

    intrepid lifev update

--- lifev/em/examples/example_EMHeart/EssentialPatchBCCircularSmooth.hpp
+++ lifev/em/examples/example_EMHeart/EssentialPatchBCCircularSmooth.hpp
@@ -1,5 +1,5 @@
 //
-//  EssentialPatchBCCircularSmooth
+//  EssentialPatchBCCircularSmooth.hpp
 //  lifev-heart
 //
 //  Created by Thomas Kummer on 30.06.18.
@@ -42,53 +42,53 @@ public:
     
 protected:
     
-    virtual vectorPtr_Type directionalVectorField (const boost::shared_ptr<FESpace<RegionMesh<LinearTetra>, MapEpetra >> dFeSpace, Vector3D& direction, const Real& disp, const Real& time) const
-    {
-        vectorPtr_Type vectorField (new VectorEpetra( dFeSpace->map(), Repeated ));
-        auto nCompLocalDof = vectorField->epetraVector().MyLength() / 3;
-        
-        direction.normalize();
-        direction *= disp;
-        
-        if ( vectorField->comm().MyPID() == 0 )
-        {
-            std::cout << "Create directional " << m_Name << " directional vector field ... ";
-        }
-        
-        for (int j (0); j < nCompLocalDof; ++j)
-        {
-            // Get coordiantes
-            UInt iGID = vectorField->blockMap().GID (j);
-            UInt jGID = vectorField->blockMap().GID (j + nCompLocalDof);
-            UInt kGID = vectorField->blockMap().GID (j + 2 * nCompLocalDof);
-
-//            Vector3D coord;
+//    virtual vectorPtr_Type directionalVectorField (const boost::shared_ptr<FESpace<RegionMesh<LinearTetra>, MapEpetra >> dFeSpace, Vector3D& direction, const Real& disp, const Real& time) const
+//    {
+//        vectorPtr_Type vectorField (new VectorEpetra( dFeSpace->map(), Repeated ));
+//        auto nCompLocalDof = vectorField->epetraVector().MyLength() / 3;
 //
-//            coord(0) = dFeSpace->mesh()->point(iGID).x() + (*m_dispPtr)[iGID];
-//            coord(1) = dFeSpace->mesh()->point(iGID).y() + (*m_dispPtr)[jGID];
-//            coord(2) = dFeSpace->mesh()->point(iGID).z() + (*m_dispPtr)[kGID];
+//        direction.normalize();
+//        direction *= disp;
 //
-//            // Radial and axial distance to center line
-//            auto currentPatchCenter = m_Center + activationFunction(time) * direction;
-//            auto radialDistance = ( (coord - m_Center).cross(coord - currentPatchCenter) ).norm() / (m_Center - currentPatchCenter).norm();
-//            auto axialDistance = (coord - currentPatchCenter).dot(direction) * direction;
+//        if ( vectorField->comm().MyPID() == 0 )
+//        {
+//            std::cout << "Create directional " << m_Name << " directional vector field ... ";
+//        }
 //
-//            // If coordiantes inside or outside of a certain radius
-//            Real radiusDispFactor (0.5);
-//            auto displacement = (radiusDispFactor * disp - disp) * std::pow(radialDistance / m_Radius, 2.0) + disp;
+//        for (int j (0); j < nCompLocalDof; ++j)
+//        {
+//            // Get coordiantes
+//            UInt iGID = vectorField->blockMap().GID (j);
+//            UInt jGID = vectorField->blockMap().GID (j + nCompLocalDof);
+//            UInt kGID = vectorField->blockMap().GID (j + 2 * nCompLocalDof);
 //
-//            // If patch inside or outside the structure
+////            Vector3D coord;
+////
+////            coord(0) = dFeSpace->mesh()->point(iGID).x() + (*m_dispPtr)[iGID];
+////            coord(1) = dFeSpace->mesh()->point(iGID).y() + (*m_dispPtr)[jGID];
+////            coord(2) = dFeSpace->mesh()->point(iGID).z() + (*m_dispPtr)[kGID];
+////
+////            // Radial and axial distance to center line
+////            auto currentPatchCenter = m_Center + activationFunction(time) * direction;
+////            auto radialDistance = ( (coord - m_Center).cross(coord - currentPatchCenter) ).norm() / (m_Center - currentPatchCenter).norm();
+////            auto axialDistance = (coord - currentPatchCenter).dot(direction) * direction;
+////
+////            // If coordiantes inside or outside of a certain radius
+////            Real radiusDispFactor (0.5);
+////            auto displacement = (radiusDispFactor * disp - disp) * std::pow(radialDistance / m_Radius, 2.0) + disp;
+////
+////            // If patch inside or outside the structure
+////
+////
+////            // Scale the direction vector
+////            auto displacementVec = displacement * direction;
+//            (*vectorField)[iGID] = direction[0];
+//            (*vectorField)[jGID] = direction[1];
+//            (*vectorField)[kGID] = direction[2];
+//        }
 //
-//
-//            // Scale the direction vector
-//            auto displacementVec = displacement * direction;
-            (*vectorField)[iGID] = direction[0];
-            (*vectorField)[jGID] = direction[1];
-            (*vectorField)[kGID] = direction[2];
-        }
-        
-        return vectorField;
-    }
+//        return vectorField;
+//    }
     
     virtual const bool nodeOnPatch(Vector3D& coord) const
     {
