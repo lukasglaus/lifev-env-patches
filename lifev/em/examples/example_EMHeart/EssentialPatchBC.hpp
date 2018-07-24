5a3a1c2907727832f5e3b49436ea4967c515bcf3
thomaskummer <kummetho@gmail.com>
2018-07-24 13:46:00 +0200

    intrepid lifev update

--- lifev/em/examples/example_EMHeart/EssentialPatchBC.hpp
+++ lifev/em/examples/example_EMHeart/EssentialPatchBC.hpp
@@ -189,7 +189,7 @@ public:
     EssentialPatchBCHandler(const std::string& patchList, const GetPot& dataFile) :
         m_patchList ("solid/boundary_conditions/" + patchList),
         m_dataFile (dataFile),
-        m_patchNumber (dataFile.vector_variable_size(m_patchList))
+        m_patchNumber (( dataFile.vector_variable_size(m_patchList) ))
     {}
     
     ~EssentialPatchBCHandler(){}
