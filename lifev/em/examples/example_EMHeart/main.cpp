e2854b97020e1bd57c956cf117dad0f9457a899b
thomaskummer <kummetho@gmail.com>
2018-07-26 10:07:17 +0200

    intrepid lifev update

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -192,7 +192,7 @@ int main (int argc, char** argv)
     EssentialPatchBCHandler patchHandler ("listEssentialPatchBC", dataFile);
     patchHandler.addPatchBC(solver);
     
-    heartSolver.setPatchDisplacement(patchHandler.patchDisplacementSum());
+    heartSolver.setPatchDisplacement(*patchHandler.patchDisplacementSum());
     
     
     //============================================
