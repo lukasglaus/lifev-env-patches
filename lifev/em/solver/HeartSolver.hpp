3fdd675f7b7bbb688e680560e383e2f151af8ab6
thomaskummer <kummetho@gmail.com>
2018-07-27 11:20:00 +0200

    euler lifev update

--- lifev/em/solver/HeartSolver.hpp
+++ lifev/em/solver/HeartSolver.hpp
@@ -360,7 +360,7 @@ public:
         return traction.dot(velocity);
     }
     
-    void setPatchDisplacementSumPtr(vectorPtr_Type& patchDisplacementSumPtr)
+    void setPatchDisplacementSumPtr(vectorPtr_Type patchDisplacementSumPtr)
     {
         m_patchDisplacementSumPtr = patchDisplacementSumPtr;
     }
