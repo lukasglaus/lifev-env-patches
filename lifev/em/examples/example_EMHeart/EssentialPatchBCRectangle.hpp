63758614a2d4a421450bea5cfc7a4aa9021352df
thomaskummer <kummetho@gmail.com>
2018-09-18 10:31:33 +0200

    intrepid lifev update

--- lifev/em/examples/example_EMHeart/EssentialPatchBCRectangle.hpp
+++ lifev/em/examples/example_EMHeart/EssentialPatchBCRectangle.hpp
@@ -46,8 +46,8 @@ protected:
         coordZyl(1) = coord(1); // height
         coordZyl(2) = std::atan2(coord(2), coord(1)); // angle
 
-        angle *= m_angle * PI/180;
-        dAngle *= m_dAngle * PI/180;
+        angle = m_angle * PI/180;
+        dAngle = m_dAngle * PI/180;
 
         const bool inAngleRange ( coordZyl(2) < (angle + dAngle) && coordZyl(2) > (angle - dAngle) );
         const bool inVerticalRange ( coordZyl(1) < (m_height + m_width) && coordZyl(1) > (m_height - m_width) );
