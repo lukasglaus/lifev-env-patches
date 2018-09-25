0231611911033481d3b21cdd81ef81f00c05fb99
thomaskummer <kummetho@gmail.com>
2018-09-25 15:24:28 +0200

    euler lifev update

--- lifev/em/examples/example_EMHeart/EssentialPatchBC.hpp
+++ lifev/em/examples/example_EMHeart/EssentialPatchBC.hpp
@@ -100,7 +100,7 @@ public:
     
                     for (int k(0); k < 3; ++k)
                     {
-                        (*m_patchLocationPtr)[face.point(k).id()] = 1.0;
+                        //(*m_patchLocationPtr)[face.point(k).id()] = 1.0;
                     }
                 
                 }
