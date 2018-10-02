ea12042b298b50278477918a4493dc0e67ff33bc
thomaskummer <kummetho@gmail.com>
2018-10-02 10:03:36 +0200

    cervin lifev update

--- lifev/em/examples/example_EMHeart/EssentialPatchBC.hpp
+++ lifev/em/examples/example_EMHeart/EssentialPatchBC.hpp
@@ -115,7 +115,7 @@ public:
     
                     for (int k(0); k < 3; ++k)
                     {
-                        std::cout << p1ScalarField.size() << " / " << face.point(k).id() << " / " << face.point(k).localId() << "\n";
+                        std::cout << p1ScalarField.size() << " / " << p1ScalarField.minValue() << " / " << p1ScalarField.maxValue() << " / " << p1ScalarField.abs() << " / " << face.point(k).id() << " / " << face.point(k).localId() << "\n";
                         p1ScalarField[face.point(k).id()] = 1.0;
                         //(*m_patchLocationPtr)[face.point(k).id()] = 1.0;
                     }
